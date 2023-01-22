7a2a1b53f1bf69ef48a21d1a1b544eba8a7cf85f
saubby <saubby27@gmail.com>
2023-01-22 00:36:19 -0500

    create and update user

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -43,6 +43,22 @@ const userController = {
             .catch((err) => res.json(err));
     },
 
-    //update User by id
+    //update User
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, {
+            new: true,
+            reunValidators: true,
+        })
+            .then((dbUserData) => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: "No user found with this id!" });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch((err) => res.status(400).json(err));
+    },
+
+    //delete user
 
 }
